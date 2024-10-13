cccca2f4f0969f43f8db17dbdd204c555b9fa1a8
Yusuf Efe <yusufefe417@gmail.com>
2024-10-14 00:09:45 +0300

    yemek-tarifleri

---
+++ yemek-tarifleri/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+export default function SearchBar() {
+    const [keyword, setKeyword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        navigate(`/search?q=${keyword}`);
+    }
+
+    //local/tarifler?q=kebap
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="search" className="form-control" placeholder="Search..." onChange={(e) => setKeyword(e.target.value)} />
+        </form>
+    )
+}
---
+++ yemek-tarifleri/src/components/ThemeSelector.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import './ThemeSelector.css';
+import { ThemeContext } from '../contexts/ThemeContext';
+import LightIcon from '../assets/light.svg';
+import DarkIcon from '../assets/dark.svg'
+
+
+const themeColors = ["warning","danger","primary","success"];
+
+export default function ThemeSelector() {
+    const { changeColor, changeMode, mode } = useContext(ThemeContext);
+    const toggleMode = () => {
+        changeMode(mode === "dark" ? "light" : "dark")
+    }
+
+    return (
+        <div className="container theme-selector">
+            <div className="mode-toggle">
+                <img src={mode === "dark" ? DarkIcon : LightIcon} alt="dark light mode" onClick={toggleMode} />                  
+            </div>
+            <div className="theme-links">
+                {
+                    themeColors.map(color => (
+                        <span
+                            key={color}
+                            className={`bg-${color}`}
+                            onClick={() => changeColor(color)}></span>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
